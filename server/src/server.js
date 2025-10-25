903460ea34bc37af14db0ba921a1babc01d664d2
Atif Afsar <atifafsar70@gmail.com>
2025-10-25 21:15:40 +0530

    Connecting to Database

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
+const  connectToDatabase  = require('./db/db');
 const PORT = process.env.PORT || 8000;
 async function startServer() {
-await loadPlanetsData();
+  await connectToDatabase();
+  await loadPlanetsData();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
